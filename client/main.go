9ea46c629e01a0981ea8d06d9321a65989416890
o.b.demirpolat <burak.demirpolat@trendyol.com>
2022-09-25 11:52:49 +0300

    fix

--- client/main.go
+++ client/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/songgao/water"
+	"log"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
+	"vpntest/cmd"
 )
 
 var conn net.Conn
@@ -34,4 +37,75 @@ func main() {
 	termSignal := make(chan os.Signal, 1)
 	signal.Notify(termSignal, os.Interrupt, syscall.SIGTERM)
 	fmt.Println("closing")
+}
+
+func connectTcpServer() (net.Conn, error) {
+	conn, err := net.Dial("tcp", "89.252.131.88:8990")
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("dial ok")
+	return conn, nil
+}
+
+func listenTcpConn(iface *water.Interface, conn net.Conn) {
+	message := make([]byte, 2000)
+	for {
+		n, err := conn.Read(message)
+		if err != nil {
+			log.Println("read from connection failed:", err.Error())
+		}
+		message = message[:n]
+		cmd.WritePacket(message)
+		_, err = iface.Write(message)
+		if err != nil {
+			log.Println("write to interface failed:", err.Error())
+		}
+	}
+}
+
+func listenInterface(iface *water.Interface, conn net.Conn) {
+	fmt.Println("interface listening")
+	packet := make([]byte, 2000)
+	for {
+		n, err := iface.Read(packet)
+		if err != nil {
+			log.Println("ifce read error:", err)
+		}
+		packet = packet[:n]
+		cmd.WritePacket(packet)
+		log.Printf("Packet Received: % x\n", packet)
+		_, err = conn.Write(packet)
+		if err != nil {
+			log.Println("conn write error:", err)
+		}
+	}
+}
+
+func createTun() (*water.Interface, error) {
+	iface, err := water.New(water.Config{
+		DeviceType: water.TUN,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Interface Name: %s\n", iface.Name())
+
+	out, err := cmd.RunCommand(fmt.Sprintf("sudo ifconfig %s 10.1.0.10 10.1.0.10 up", iface.Name()))
+	if err != nil {
+		fmt.Println(out)
+		return nil, err
+	}
+
+	return iface, nil
+}
+
+func addRoute(iface *water.Interface) error {
+	out, err := cmd.RunCommand(fmt.Sprintf("route add -host 10.1.0.10 -interface %s", iface.Name()))
+	if err != nil {
+		fmt.Println(out)
+		return err
+	}
+	return nil
 }
--- server/main.go
+++ server/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/songgao/water"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"vpntest/cmd"
 )
 
 func main() {
@@ -51,4 +54,73 @@ func runTestServer2() {
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+var conn net.Conn
+
+func createListener() (net.Listener, error) {
+	return net.Listen("tcp", "89.252.131.88:8990")
+}
+
+func listenTCP(listener net.Listener, iface *water.Interface) {
+	for {
+		fmt.Println("tcp connection listening")
+		var err error
+		conn, err = listener.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn, iface)
+	}
+}
+func handle(conn net.Conn, iface *water.Interface) {
+	message := make([]byte, 2000)
+	n, err := conn.Read(message)
+	if err != nil {
+		log.Println("conn read error:", err)
+	}
+	message = message[:n]
+	cmd.WritePacket(message)
+	if iface != nil {
+		_, err = iface.Write(message)
+		if err != nil {
+			log.Println("ifce write err:", err)
+		}
+	}
+}
+
+func listenInterface(iface *water.Interface) {
+	fmt.Println("interface listening")
+	packet := make([]byte, 2000)
+	for {
+		n, err := iface.Read(packet)
+		if err != nil {
+			log.Println("ifce read error:", err)
+		}
+		packet = packet[:n]
+		cmd.WritePacket(packet)
+		log.Printf("Packet Received: % x\n", packet)
+		_, err = conn.Write(packet)
+		if err != nil {
+			log.Println("conn write error:", err)
+		}
+	}
+}
+
+func createTun(ip string) (*water.Interface, error) {
+	config := water.Config{
+		DeviceType: water.TUN,
+	}
+
+	iface, err := water.New(config)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Interface Name: %s\n", iface.Name())
+	out, err := cmd.RunCommand(fmt.Sprintf("sudo ip addr add %s/24 dev %s", ip, iface.Name()))
+	if err != nil {
+		fmt.Println(out)
+		return nil, err
+	}
+	return iface, nil
 }
